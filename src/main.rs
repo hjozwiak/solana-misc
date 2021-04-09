016000f43227fcf9eeb8c9a2efc2140c03cc03b7
hunter Jozwiak <hunter.t.joz@gmail.com>
2021-04-09 12:02:56 -0400

    Add a program to trace macro expansion with.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use solana_program::pubkey::Pubkey;
+
+/// A sample struct with which to play.
+#[derive(Debug)]
+pub struct AddressBookEntry {
+    name: String,
+    pay_to: Pubkey,
+}
+impl AddressBookEntry {
+    fn new(name: String, address: Pubkey) -> Self {
+        Self {
+            name: name,
+            pay_to: address,
+        }
+    }
+}
+pub fn main() {
+    let payto = AddressBookEntry::new("Richard".to_string(), Pubkey::new_unique());
+    println!("{:#?}", payto);
 }
