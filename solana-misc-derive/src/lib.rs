ebfce83f4e29b900eb3dab4263ec9dc21533f9d8
hunter Jozwiak <hunter.t.joz@gmail.com>
2021-04-09 10:10:49 -0400

    Add dependencies and set up the procedural macro.

--- solana-misc-derive/src/lib.rs
+++ solana-misc-derive/src/lib.rs
@@ -1,7 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate proc_macro;
+use proc_macro::TokenStream;
+use quote::quote;
+use solana_program::pubkey::Pubkey;
+use syn::{parse_macro_input, ExprStruct, Ident};
+
+/// Given a struct that has a field of type Pubkey, find this member, evaluate the expression, decode it as base58, and ensure that it is equal to a given string.
+#[proc_macro]
+pub fn ensure_encodings_are_equal(input: TokenStream) -> TokenStream {
+    unimplemented!();
 }
