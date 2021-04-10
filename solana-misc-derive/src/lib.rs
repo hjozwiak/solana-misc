5158a07524dea23652aff6e804ac68ad5e4daab8
hunter Jozwiak <hunter.t.joz@gmail.com>
2021-04-10 14:50:44 -0400

    Fix bugs with deriving, define the trait. For now, it shows the number of fields that match the type solana_program::pubkey::Pubkey but it is broken for some reason.

--- solana-misc-derive/src/lib.rs
+++ solana-misc-derive/src/lib.rs
@@ -3,30 +3,43 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
 use solana_program::pubkey::Pubkey;
-use syn::spanned::Spanned;
-use syn::{parse, parse_str, Fields, Item, ItemStruct, Type, TypePath};
-#[proc_macro_attribute]
-pub fn keys_match(meta: TokenStream, input: TokenStream) -> TokenStream {
-    let path: TypePath = match parse_str("solana_program::pubkey::Pubkey") {
-        Ok(p) => p,
-    };
+use syn::{parse, parse_str, Data, DataStruct, DeriveInput, Fields, Type, TypePath};
+#[proc_macro_derive(KeysMatch)]
+pub fn keys_match(input: TokenStream) -> TokenStream {
+    let ast: DeriveInput = syn::parse(input).unwrap();
 
-    let the_item: Item = syn::parse(input).expect("Failed to parse.");
-    match the_item {
-        Item::Struct(ref the_struct) => {
-            // Determine if there are any fields of type solana_program::pubkey in this struct
-            // If there is more than one, returnan error since it is unclear what to do with them
-            // For simplicity, make sure the struct is of named fields
+    // Determine if there are any fields of type solana_program::pubkey in this struct
+    // If there is more than one, returnan error since it is unclear what to do with them
+    // For simplicity, make sure the struct is of named fields
+    match ast.data {
+        Data::Struct(ref the_struct) => {
+            let name = ast.ident;
+
+            let matches = count_fields(the_struct);
+            let gen = quote! {
+                impl KeysMatch for #name {
+                    fn num_matches() -> usize {
+                        #matches
+                    }
+                }
+            };
+            gen.into()
         }
         _ => panic!(),
     }
 }
-fn count_fields(the_struct: &ItemStruct, path: TypePath) -> usize {
+fn count_fields(the_struct: &DataStruct) -> usize {
+    let path: TypePath = match parse_str("solana_program::pubkey::Pubkey") {
+        Ok(p) => p,
+        Err(e) => panic!(),
+    };
+
     match the_struct.fields {
         Fields::Named(ref fields) => fields
             .named
             .iter()
-            .filter(|field| field.ty == Type::Path(path))
+            .filter(|field| field.ty == Type::Path(path.clone()))
             .count(),
+        _ => 0,
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
 #[macro_use]
-extern crate keys_match;
+extern crate solana_misc_derive;
 use solana_program::pubkey::Pubkey;
 /// A sample struct with which to play.
-#[keys_match]
-#[derive(Debug)]
+trait KeysMatch {
+    fn num_matches() -> usize;
+}
+#[derive(KeysMatch)]
 pub struct AddressBookEntry {
     name: String,
     pay_to: Pubkey,
 }
+
 impl AddressBookEntry {
     fn new(name: String, address: Pubkey) -> Self {
         Self {
-            name: name,
+            name,
             pay_to: address,
         }
     }
 }
 pub fn main() {
     let payto = AddressBookEntry::new("Richard".to_string(), Pubkey::new_unique());
-    let b58 = bs58::encode(payto.pay_to.to_bytes()).into_string();
+    let _b58 = bs58::encode(payto.pay_to.to_bytes()).into_string();
 }
