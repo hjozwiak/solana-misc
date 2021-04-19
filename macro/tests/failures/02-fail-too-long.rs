ebe6ce5fb5cac7afa1840223391864904f060916
hunter Jozwiak <hunter.t.joz@gmail.com>
2021-04-19 15:53:02 -0400

    Add the test case for when a string is too long.

---
+++ macro/tests/failures/02-fail-too-long.rs
@@ -0,0 +1,16 @@
+#[macro_use]
+extern crate solana_misc_macro;
+use solana_program::pubkey::Pubkey;
+struct Tester {
+    key: Pubkey,
+}
+
+fn main() {
+    let test = Tester {
+        key: Pubkey::new_unique(),
+    };
+    assert_pubkeys_equal!(
+        test.key,
+        "my1111111111111111111111111111111111111111111111111111"
+    );
+}
--- macro/tests/runner.rs
+++ macro/tests/runner.rs
@@ -3,4 +3,6 @@ fn runner() {
     let t = trybuild::TestCases::new();
     // Failure case 1, the array is too short.
     t.compile_fail("tests/failures/01-fail-too-short.rs");
+    // Fail to compile when the array is too long.
+    t.compile_fail("tests/failures/02-fail-too-long.rs");
 }
