36cd22b47260403790339a26084be6e853719851
hunter Jozwiak <hunter.t.joz@gmail.com>
2021-04-19 16:27:20 -0400

    More tests.

--- macro/tests/runner.rs
+++ macro/tests/runner.rs
@@ -5,4 +5,6 @@ fn runner() {
     t.compile_fail("tests/failures/01-fail-too-short.rs");
     // Fail to compile when the array is too long.
     t.compile_fail("tests/failures/02-fail-too-long.rs");
+    t.compile_fail("tests/failures/03-fail-decode.rs");
+    t.compile_fail("tests/failures/04-fail-field.rs")
 }
