208db984a94bb94369719115c65116cc45b73b89
hunter Jozwiak <hunter.t.joz@gmail.com>
2021-04-20 10:20:51 -0400

    Fix up tests, orginize them out in to separate functions.

--- macro/tests/runner.rs
+++ macro/tests/runner.rs
@@ -1,5 +1,14 @@
+// Testing conditions for the procedural macro.
+// There are four failure cases of which I am aware:
+//1) The array is too short
+// 2) The array is too long
+// 3) There was some other failure in decoding
+// 4) The first argument is not a struct field
+// 5) The success case is when both keys are equal
+//6) The partial success case is a successful compilation but the program panicked because they keys were not equal
+
 #[test]
-fn runner() {
+fn run_failures() {
     let t = trybuild::TestCases::new();
     // Failure case 1, the array is too short.
     t.compile_fail("tests/failures/01-fail-too-short.rs");
@@ -9,9 +18,17 @@ fn runner() {
     t.compile_fail("tests/failures/03-fail-decode.rs");
     // Fail to compile when our first argument is not a field access expression
     t.compile_fail("tests/failures/04-fail-field.rs");
-    // Succeed when both keys are equal.
+}
+
+#[test]
+fn run_success() {
+    let t = trybuild::TestCases::new();
     t.pass("tests/successes/05-succeed.rs");
-    #[should_panic]
-    // Technically a success, but will panic at runtime since the keys are unequal
-    t.pass("tests/successes/06-barf-when-unequal.rs");
+}
+
+#[test]
+#[should_panic]
+fn run_partial_success() {
+    let t = trybuild::TestCases::new();
+    t.pass("06-barf-when-unequal.rs");
 }
