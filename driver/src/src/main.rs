636ecf02ca55f826b2439094d6442250a8483a07
Hunter Jozwiak <hunter.t.joz@gmail.com>
2021-06-10 13:05:42 -0400

    Clean up .github and add the new driver package.

---
+++ driver/src/src/main.rs
@@ -0,0 +1,26 @@
+#[macro_use]
+extern crate solana_misc_macro;
+use solana_program::pubkey::Pubkey;
+use std::str::FromStr;
+
+/// A sample struct with which to play.
+pub struct AddressBookEntry {
+    name: String,
+    pay_to: Pubkey,
+}
+
+impl AddressBookEntry {
+    fn new(name: String, address: Pubkey) -> Self {
+        Self {
+            name,
+            pay_to: address,
+        }
+    }
+}
+pub fn main() {
+    let payto = AddressBookEntry::new(
+        "Richard".to_string(),
+        Pubkey::from_str("Fd7btgySsrjuo25CJCj7oE7VPMyezDhnx7pZkj2v69Nk").unwrap(),
+    );
+    assert_pubkeys_equal!(payto.pay_to, "Fd7btgySsrjuo25CJCj7oE7VPMyezDhnx7pZkj2v69Nk");
+}
